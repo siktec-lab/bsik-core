7fa4d2ad791bd998a5c3af0fbda447cb0d73a1dd
Shlomo Hassid <shlomohassid@gmail.com>
2023-02-05 19:24:07 +0200

    This will cary the information about the current Bsik Framework

---
+++ src/About.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace Siktec\Bsik;
+
+/**
+ *  About class is used to store information about the bsik framework.
+ */
+class About {
+
+    public const NAME        = "BSIK";
+
+    public const DESCRIPTION = "A PHP framework for building web applications.";
+
+    public const AUTHOR      = "SIKTEC";
+
+    public const LICENSE     = "MIT";
+
+    public const CONTACT     = "https://www.siktec.net";
+
+    public const VERSION     = "1.0.1";
+
+}
