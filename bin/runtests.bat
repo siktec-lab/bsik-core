3c0a7754179cde936f55b0f743126c38aeda6bdc
Shlomo Hassid <shlomohassid@gmail.com>
2023-02-07 17:03:49 +0200

    fixed test runner bat

--- bin/runtests.bat
+++ bin/runtests.bat
@@ -2,7 +2,8 @@
 :: --------------------------------------------------------
 :: Run tests of BSIK Framework
 :: --------------------------------------------------------
-SET UNITTESTTARGET=%~dp0/../vendor/phpunit/phpunit/phpunit
+SET UNITTESTTARGET=%~dp0/../vendor/bin/phpunit
+
 if "%1"=="" ( 
     goto fulltest 
 ) else ( 
@@ -11,13 +12,13 @@ if "%1"=="" (
 
 :specific
 
-if exist %~dp0/../manage/tests/%1Test.php (
+if exist %~dp0/../tests/%1Test.php (
     echo - Running BSIK Test - %1Test
-    php "%UNITTESTTARGET%" --testdox manage/tests/%1Test.php
+    php "%UNITTESTTARGET%" --testdox --color="auto" tests/%1Test.php
 ) else (
     if exist %~dp0/../manage/tests/%1.php (
         echo - Running BSIK Test - %1
-        php "%UNITTESTTARGET%" --testdox manage/tests/%1.php
+        php "%UNITTESTTARGET%" --testdox --color="auto" tests/%1.php
     ) else (
         echo Test not found check your spelling and use CI name.
     )
@@ -27,5 +28,5 @@ goto :eof
 :fulltest
 
 echo - Running BSIK Tests...
-php "%UNITTESTTARGET%" --testdox manage/tests
+php "%UNITTESTTARGET%" --testdox --color="auto" tests
 goto :eof
