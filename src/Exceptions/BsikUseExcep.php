3b55ecb40be56425d2a3cba8ae8446b30a6c8e92
Shlomo Hassid <shlomohassid@gmail.com>
2023-02-07 17:03:30 +0200

    Improved Exception class names

--- src/Exceptions/BsikUseExcep.php
+++ src/Exceptions/BsikUseExcep.php
@@ -7,20 +7,22 @@ if (!defined("E_PLAT_NOTICE"))              define("E_PLAT_NOTICE",             
 if (!defined("E_PLAT_WARNING"))             define("E_PLAT_WARNING",            E_USER_WARNING);
 if (!defined("E_PLAT_ERROR"))               define("E_PLAT_ERROR",              E_USER_ERROR);
 
+use \Siktec\Bsik\Exceptions;
+
 class BsikUseExcep {
 
-    public static function init($class, $file = null)
+    public static function init()
     {
         //Register:
         if (USE_BSIK_ERROR_HANDLERS && !defined('PREVENT_BSIK_ERROR_HANDLERS')) {
             set_exception_handler(
-                ['Bsik\Exceptions\FrameWorkExcepHandler',"handleException"]
+                [Exceptions\FrameWorkExcepHandler::class, "handleException"]
             );
             set_error_handler(
-                ['Bsik\Exceptions\FrameWorkExcepHandler', "handleError"], 
+                [Exceptions\FrameWorkExcepHandler::class, "handleError"], 
                 E_ALL
             );
-            register_shutdown_function(['Bsik\Exceptions\FrameWorkExcepHandler', "handleFatalShutdown"]);
+            register_shutdown_function([Exceptions\FrameWorkExcepHandler::class, "handleFatalShutdown"]);
         }
     }
 }
